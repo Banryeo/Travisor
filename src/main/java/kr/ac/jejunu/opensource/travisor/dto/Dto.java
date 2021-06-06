75ddee4203f7a9d6e9fd0791c8bab1763006a864
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-06 19:23:16 +0900

    Dto, Model 설정

--- src/main/java/kr/ac/jejunu/opensource/travisor/dto/Dto.java
+++ src/main/java/kr/ac/jejunu/opensource/travisor/dto/Dto.java
@@ -1,4 +1,14 @@
 package kr.ac.jejunu.opensource.travisor.dto;
 
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Dto {
+    private String culture;
+    private String startDate;
+    private String endDate;
+    private String location;
 }
--- src/main/java/kr/ac/jejunu/opensource/travisor/model/Model.java
+++ src/main/java/kr/ac/jejunu/opensource/travisor/model/Model.java
@@ -1,14 +1,39 @@
 package kr.ac.jejunu.opensource.travisor.model;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.GeneratorType;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Model {
 
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    @NotNull
+    private String cultureName;
+
+    @Column
+    @NotNull
+    private String startDate;
+
+    @Column
+    @NotNull
+    private String endDate;
+
+    @Column
+    @NotNull
+    private String location;
+
+    @Column
+    @NotNull
+    private String description;
+
+
+
 }

303f70f63400d0755f84ac9dc3c437a3494b977e
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-11 23:42:55 +0900

    주소 변경

--- src/main/java/kr/ac/jejunu/opensource/travisor/context/Context.java
+++ src/main/java/kr/ac/jejunu/opensource/travisor/context/Context.java
@@ -144,7 +144,7 @@ public class Context {
 
         group2.put("action", "webLink");
         group2.put("label", "설명 자세히 보기");
-        group2.put("webLinkUrl", "http://3.35.24.12/test/api/" + itemId);
+        group2.put("webLinkUrl", "http://3.35.24.12./index.html/?id=" + itemId);
 
         group3.put("action", "webLink");
         group3.put("label", "구글 검색하기");

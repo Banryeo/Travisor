cd580228747dc7f0bf77a474b232481e7479bf49
hunko <rhwlgns4386@naver.com>
2021-06-11 23:28:14 +0900

    get

--- src/main/java/kr/ac/jejunu/opensource/travisor/controller/api/ApiController.java
+++ src/main/java/kr/ac/jejunu/opensource/travisor/controller/api/ApiController.java
@@ -1,4 +1,20 @@
 package kr.ac.jejunu.opensource.travisor.controller.api;
 
+import kr.ac.jejunu.opensource.travisor.model.Model;
+import kr.ac.jejunu.opensource.travisor.repository.Repository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ApiController {
+
+    public final Repository repository;
+
+    @GetMapping("/test/api/{modelId}")
+    public Model getModel(@PathVariable("modelId") Integer id){
+        return repository.findById(id).get();
+    }
 }

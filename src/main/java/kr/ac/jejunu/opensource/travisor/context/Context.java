e66f62061cf1ad2597022bd8a9335f11037d13ae
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-11 22:45:34 +0900

    text삭제

--- src/main/java/kr/ac/jejunu/opensource/travisor/context/Context.java
+++ src/main/java/kr/ac/jejunu/opensource/travisor/context/Context.java
@@ -142,9 +142,8 @@ public class Context {
         HashMap<String, Object> group2 = new HashMap<String, Object>();
         HashMap<String, Object> group3 = new HashMap<String, Object>();
 
-        group2.put("action", "block");
-        group2.put("label", "설명 자세히 보기");
-        group2.put("blockId", reponseBlockId);
+        group2.put("action", "message");
+        group2.put("label", itemDescription);
 
         group3.put("action", "webLink");
         group3.put("label", "구글 검색하기");
